156879c74b06c9ff25d395ee4fac54c343ee3d09
UNBREAKING <apaseishvili@gmail.com>
2017-01-30 20:29:12 +0300

    1 of 7

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,21 @@
 class TicTacToe {
     constructor() {
-
+        this.CurrentPlayerSymbol='x';
+        this.matrix=[[null,null,null],[null,null,null],[null,null,null]];
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.CurrentPlayerSymbol;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if(this.CurrentPlayerSymbol==='x' && this.matrix[rowIndex][columnIndex]===null){
+            this.matrix[rowIndex][columnIndex]=this.CurrentPlayerSymbol;
+            this.CurrentPlayerSymbol='o';
+        }else if(this.matrix[rowIndex][columnIndex]===null){
+            this.matrix[rowIndex][columnIndex]=this.CurrentPlayerSymbol;
+            this.CurrentPlayerSymbol='x';
+        }
     }
 
     isFinished() {
