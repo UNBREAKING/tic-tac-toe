f3e014e40448d236195cf62a73629ac29c2dbe1b
UNBREAKING <apaseishvili@gmail.com>
2017-01-31 00:33:15 +0300

    7 of 7

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -2,6 +2,7 @@ class TicTacToe {
     constructor() {
         this.CurrentPlayerSymbol='x';
         this.matrix=[[null,null,null],[null,null,null],[null,null,null]];
+        this.winner=null;
     }
 
     getCurrentPlayerSymbol() {
@@ -19,19 +20,54 @@ class TicTacToe {
     }
 
     isFinished() {
-
+        if(this.isDraw()===true || this.winner!=null){
+            return true;
+        }else{
+            return false
+        }
     }
 
     getWinner() {
-
+        this.isDraw();
+        return this.winner;
     }
 
     noMoreTurns() {
 
+      if(this.matrix[0].indexOf(null)=== -1 && this.matrix[1].indexOf(null) ===-1 && this.matrix[2].indexOf(null)===-1){
+         return true;
+     }else{
+         return false;
+     }
     }
 
     isDraw() {
-
+        if(this.matrix[0][0]===this.matrix[0][1] && this.matrix[0][0]===this.matrix[0][2]){
+            this.winner=this.matrix[0][0];
+        }else if(this.matrix[1][0]===this.matrix[1][1] && this.matrix[1][0]===this.matrix[1][2])
+        {
+            this.winner=this.matrix[1][0];
+        }else if(this.matrix[2][0]===this.matrix[2][1] && this.matrix[2][0]===this.matrix[2][2])
+        {
+            this.winner=this.matrix[2][0];
+        }else if(this.matrix[0][0]===this.matrix[1][0] && this.matrix[0][0]===this.matrix[2][0])
+        {
+            this.winner=this.matrix[0][0];
+        }else if(this.matrix[0][1]===this.matrix[1][1] && this.matrix[0][1]===this.matrix[2][1])
+        {
+            this.winner=this.matrix[0][1];
+        }else if(this.matrix[0][2]===this.matrix[1][2] && this.matrix[0][2]===this.matrix[2][2])
+        {
+            this.winner=this.matrix[0][2];
+        }else if(this.matrix[0][0]===this.matrix[1][1] && this.matrix[0][0]===this.matrix[2][2]) {
+            this.winner = this.matrix[0][0];
+        }else if(this.matrix[2][0]===this.matrix[1][1] && this.matrix[2][0]===this.matrix[0][2])
+        {
+            this.winner=this.matrix[2][0];
+        }
+        if(this.noMoreTurns()===true && this.winner===null){
+            return true;
+        }else{return false;}
     }
 
     getFieldValue(rowIndex, colIndex) {
